896e6014a52c526743923082e0036976469817e7
tulioperdigao <116309232+tulioperdigao@users.noreply.github.com>
2026-01-20 10:40:12 -0300

    FEAT: implementa cenário de testes para controlador de usuários com funcionalidades de listar e criar usuários. - Adiciona mocks para execução dos testes. - Modificações pontuais no tsconfig para build.

---
+++ dist/database.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.database = void 0;
+const database = [
+    "Tulio",
+    "Kaio",
+    "Rafael",
+    "Gabriel"
+];
+exports.database = database;
--- src/controllers/usersController.test.ts
+++ src/controllers/usersController.test.ts
@@ -1,5 +1,35 @@
+import { makeMockResponse } from "../mocks/mockResponse";
+import { UsersController } from "./usersController"
+import { Request } from "express";
+
 describe('Users Controller', () => {
-    it('Deve somar 1 + 1', () => {
-        expect(1 + 1).toBe(2)
+    const usersController = new UsersController;
+
+    const mockRequest = {} as Request;
+    const mockResponse = makeMockResponse();
+    it('Deve listar os usuários cadastrados', () => {
+        usersController.listarUsuarios(mockRequest, mockResponse);
+        expect(mockResponse.state.status).toBe(200);
+        expect(mockResponse.state.json).toHaveLength(4)
+    })
+
+    it('Deve criar um novo usuário', () => {
+        mockRequest.body = {
+            name:'Novo usuário'
+        }
+
+        usersController.criarUsuario(mockRequest, mockResponse);
+        expect(mockResponse.state.status).toBe(201);
+        expect(mockResponse.state.json).toMatchObject({'mensagem' : `Usuário Novo usuário criado.`})
+    })
+
+    it('Não deve criar um usuário com o nome em branco', () => {
+        mockRequest.body = {
+            name: ''
+        }
+
+        usersController.criarUsuario(mockRequest, mockResponse);
+        expect(mockResponse.state.status).toBe(403);
+        expect(mockResponse.state.json).toMatchObject({mensagem: 'Não é possível criar usuários sem um nome.'})
     })
 })
